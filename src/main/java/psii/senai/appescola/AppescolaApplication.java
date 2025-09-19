f329ca2a05c25836ef1beb90580cca24611e23db
brunoropelato-lgtm <bruno_ropelato@estudante.sesisenai.org.br>
2025-09-19 13:21:20 -0300

    Criando_AlunoController
    criando o arquivo AlunoController

---
+++ src/main/java/psii/senai/appescola/AppescolaApplication.java
@@ -0,0 +1,13 @@
+package psii.senai.appescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppescolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppescolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psii/senai/appescola/AppescolaApplicationTests.java
@@ -0,0 +1,13 @@
+package psii.senai.appescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppescolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
